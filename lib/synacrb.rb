f7230cb356aa55d0abad43cfcb8e69dda802a154
jD91mZM2 <me@krake.one>
2017-11-22 21:32:24 +0100

    Connection logic

--- lib/synacrb.rb
+++ lib/synacrb.rb
@@ -1,5 +1,43 @@
+require "msgpack"
+require "openssl"
+require "socket"
 require "synacrb/version"
 
 module Synacrb
-  # Your code goes here...
+    class Session
+        # Connect to the server
+        def initialize(addr, hash, &callback)
+            puts "1"
+            tcp = TCPSocket.new addr, 8439
+            puts "2"
+
+            @hash = hash
+
+            context = OpenSSL::SSL::SSLContext.new
+            if callback.nil?
+                callback = method(:verify_callback)
+            end
+            context.verify_callback = callback
+            context.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+            puts "5"
+            @stream = OpenSSL::SSL::SSLSocket.new tcp, context
+            @stream.connect
+        end
+        # OpenSSL verify callback used by initialize when optional callback argument isn't set.
+        def verify_callback(_, cert)
+            pem = cert.current_cert.public_key.to_pem
+            sha256 = OpenSSL::Digest::SHA256.new
+            hash = sha256.digest(pem).unpack("H*")
+
+            hash[0].casecmp(@hash).zero?
+        end
+
+
+
+        # Close the connection
+        def close()
+            @stream.close
+        end
+    end
 end
